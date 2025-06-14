752fbfd9fabc082400b266c6c8b41a3fe8f85c01
Yogesh-325 <yogesh05032005p@gmail.com>
2025-06-14 15:29:38 +0000

    files config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+5+6) // this alert doesn't works , we are using node js not browser
+
+console.log(1+3+23+2); 
+
+console.log('hii')
+
+let name = "Yogesh";
+let age = 20;
+
+let isLoggedIn = false
+
+// number ki range bhi hai => 2 to power 53
+// bogint
+// string => '' ""
+// boolean => true/false
+// null => standalone value
+// undefined => value assign nahi hui hai
+// symbol => uniqueness
+
+
+// objects
+
+console.log(typeof null) // object 
+console.log(typeof undefined) // undefined
