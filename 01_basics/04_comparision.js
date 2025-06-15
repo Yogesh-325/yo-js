7486c4751eb9844b099aaa8b58810b818912bc9d
Yogesh-325 <yogesh05032005p@gmail.com>
2025-06-15 10:31:58 +0000

    comparision operations are done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+console.log("2" > 1);
+console.log("2" > "1");
+
+console.log(null == 0); //false
+console.log(null >= 0); //true
+console.log(null > 3); //false
+console.log(null > "abc"); //false
+
+// console.log(undefined == 0) in all comparision of undefined with zero it always give false
+
+console.log("2" === 2) // gives false because => it compare values and datatypes both
